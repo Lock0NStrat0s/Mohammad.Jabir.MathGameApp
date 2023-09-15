25214aaa2e7da3983cd658e63b71bddb9d0a6e2f
Mohammad Jabir <mohammadj94@gmail.com>
2023-09-15 13:24:26 -0600

    Added the difficulty method.

--- MathGame/Methods.cs
+++ MathGame/Methods.cs
@@ -11,7 +11,7 @@ public static class Methods
     /// <summary>
     /// Greet user and show menu
     /// </summary>
-    public static void DisplayWelcome()
+    public static string DisplayWelcome()
     {
         Console.WriteLine("Welcome to Math Game!");
         Console.WriteLine("Here you will pick the type of operation you want to answer.");
@@ -24,12 +24,30 @@ public static class Methods
         Console.WriteLine("5. Random");
         Console.WriteLine("6. History of Games");
         Console.WriteLine("Any other key to exit");
+
+        return Console.ReadLine();
     }
 
     /// <summary>
     /// Select difficulty level
     /// </summary>
-    public static void Difficulty()
+    public static string Difficulty()
+    {
+        Console.WriteLine("Pick your level of difficulty:");
+        Console.WriteLine("1. Easy - Up to and including 5x table");
+        Console.WriteLine("2. Medium - Up to and including 10x table");
+        Console.WriteLine("3. Hard - Up to and including 15x table");
+        Console.WriteLine("Any other key to return to main menu.");
+
+        return Console.ReadLine();
+    }
+
+    public static string NumOfQuestions()
+    {
+
+    }
+
+    public static void GameState()
     {
 
     }
